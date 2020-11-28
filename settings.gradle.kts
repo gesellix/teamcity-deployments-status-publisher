ffb56330743b249025353501eb50df883230c65a
Tobias Gesellchen <tobias@gesellix.de>
2020-11-28 21:05:50 +0100

    Enable Gradle's local build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,10 @@ rootProject.name = "deployments-status-teamcity-plugin"
 include("github-client")
 include("deployment-status-publisher-common")
 include("deployment-status-publisher-server")
+
+buildCache {
+  val isCiServer = System.getenv().containsKey("CI")
+  local {
+    isEnabled = !isCiServer
+  }
+}
